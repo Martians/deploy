ca3dac610d70c09aed898f07990170db52a8948e
liudong <liudong@daowoo.com>
2018-05-03 20:50:43 +0800

    fix

--- service/0_config/macro.sh
+++ service/0_config/macro.sh
@@ -4,7 +4,7 @@
 #############################################################################
 # 将最新的 source 目录挂载到 docker中
 # 将最新的 docker 脚本目录挂载到 docker中
-GLOBAL_MACRO="-v /home/long/source:/source -v /mnt/disk/docker:/docker"
+GLOBAL_MACRO="-v /home/long/source:/source -v $(dirname $BASE_PATH):/docker"
 
 SYSTMD="--privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup"
 INITIAL=/usr/sbin/init
